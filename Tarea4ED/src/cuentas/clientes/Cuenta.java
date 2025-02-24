8100a83dccaf947792be4dc09ba4ada1220e540a
alber <alber@DESKTOP-AV5QAN6>
2025-02-25 00:07:41 +0100

    Comentarios en Javadoc

--- Tarea4ED/src/cuentas/clientes/Cuenta.java
+++ Tarea4ED/src/cuentas/clientes/Cuenta.java
@@ -1,104 +1,150 @@
 package cuentas.clientes;
 
+/**
+ * Clase que representa una cuenta bancaria.
+ * 
+ * @autor Alberto Pérez López-Menchero
+ * @version 1.3
+ * @since 25-02-2025
+ */
 public class Cuenta {
 
-    private String apellidos; //Esta linea se añade para comprobar los commit y push v1.1
+    private String apellidos; // Esta línea se añade para comprobar los commit y push v1.1
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInteres;
-    private double facturaLuz; //Nueva linea añadida v1.2
+    private double facturaLuz; // Nueva línea añadida v1.2
 
-    public Cuenta()
-    {
-        nombre ="";
-        apellidos =""; //Nueva linea añadida v1.3
-        cuenta="";
-        saldo=0;
-        tipoInteres=0;
-        facturaLuz; //Nueva linea añadida v1.3
+    /**
+     * Constructor por defecto de la clase Cuenta.
+     */
+    public Cuenta() {
+        nombre = "";
+        apellidos = ""; // Nueva línea añadida v1.3
+        cuenta = "";
+        saldo = 0;
+        tipoInteres = 0;
+        facturaLuz = 0; // Nueva línea añadida v1.3
     }
 
-    public Cuenta(String nom, String cue, double sal, double tipo)
-    {
-        nombre =nom;
-        cuenta=cue;
-        saldo=sal;
-        tipoInteres=tipo;
+    /**
+     * Constructor con parámetros de la clase Cuenta.
+     * 
+     * @param nom   Nombre del titular de la cuenta.
+     * @param cue   Número de la cuenta.
+     * @param sal   Saldo inicial de la cuenta.
+     * @param tipo  Tipo de interés de la cuenta.
+     */
+    public Cuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+        tipoInteres = tipo;
     }
 
-    public double estado()
-    {
+    /**
+     * Devuelve el saldo actual de la cuenta.
+     * 
+     * @return Saldo actual de la cuenta.
+     */
+    public double estado() {
         return getSaldo();
     }
 
-    public void ingresar(double cantidad) throws Exception
-    {
-        if (cantidad<0)
+    /**
+     * Ingresa una cantidad en la cuenta.
+     * 
+     * @param cantidad Cantidad a ingresar.
+     * @throws Exception Si la cantidad es negativa.
+     */
+    public void ingresar(double cantidad) throws Exception {
+        if (cantidad < 0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
 
-    public void retirar(double cantidad) throws Exception
-    {
+    /**
+     * Retira una cantidad de la cuenta.
+     * 
+     * @param cantidad Cantidad a retirar.
+     * @throws Exception Si la cantidad es negativa o no hay suficiente saldo.
+     */
+    public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
-            throw new Exception ("No se puede retirar una cantidad negativa");
-        if (estado()< cantidad)
-            throw new Exception ("No se hay suficiente saldo");
+            throw new Exception("No se puede retirar una cantidad negativa");
+        if (estado() < cantidad)
+            throw new Exception("No hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
 
     /**
-     * @return the nombre
+     * Obtiene el nombre del titular de la cuenta.
+     * 
+     * @return Nombre del titular.
      */
     public String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Establece el nombre del titular de la cuenta.
+     * 
+     * @param nombre Nombre del titular.
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Obtiene el número de la cuenta.
+     * 
+     * @return Número de la cuenta.
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Establece el número de la cuenta.
+     * 
+     * @param cuenta Número de la cuenta.
      */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Obtiene el saldo de la cuenta.
+     * 
+     * @return Saldo de la cuenta.
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Establece el saldo de la cuenta.
+     * 
+     * @param saldo Saldo de la cuenta.
      */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInteres
+     * Obtiene el tipo de interés de la cuenta.
+     * 
+     * @return Tipo de interés.
      */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
     /**
-     * @param tipoInteres the tipoInteres to set
+     * Establece el tipo de interés de la cuenta.
+     * 
+     * @param tipoInteres Tipo de interés.
      */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
